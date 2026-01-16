eda40b77e78e162023e0d30d9aa43f7e00001b2f
AnupriyaRajaram <55028365+AnupriyaRajaram@users.noreply.github.com>
2026-01-16 11:42:55 +0530

    Add files via upload
    day 1

---
+++ 01-classroomactivities_Anupriya.js
@@ -0,0 +1,20 @@
+let firstname ='Anupriya'
+console.log(firstname);
+console.log(typeof firstname);
+
+let Companyname =`Perficient PVT ltd`
+console.log(Companyname);
+console.log(typeof Companyname);
+
+let mobilenumber = 9551668909090909
+console.log(mobilenumber);
+console.log(typeof mobilenumber);
+
+let isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+let hasplaywright
+console.log(hasplaywright);
+console.log(typeof hasplaywright);
+
