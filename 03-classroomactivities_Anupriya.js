59e6f19e4ee9e705a15817a81f556b6a73db2a7e
AnupriyaRajaram <55028365+AnupriyaRajaram@users.noreply.github.com>
2026-01-21 13:52:49 +0530

    Update 03-classroomactivities_Anupriya.js

--- 03-classroomactivities_Anupriya.js
+++ 03-classroomactivities_Anupriya.js
@@ -1,4 +1,4 @@
-
+ 
 //` Create a JavaScript function that accepts a string, reverses it, and checks if the reversed string is a 
 //palindrome, then test your function with various strings and print the results. 
 let companyname = "madam"
@@ -79,3 +79,43 @@ getUserData(callback)
 function callback(){
 console.log("Call Back Function” after 3 seconds...");
 }
+
+ 
+
+ 
+
+
+//Home Assignment: String
+//Example 1: 
+let s =  "Hello World" 
+let splitedwords = s.split(/\s+/);
+let lastword=splitedwords[splitedwords.length-1]
+let length=lastword.length
+
+console.log(length);
+
+//Example 2:
+let a = " fly me   to   the moon"
+
+let splitedwords2 = a.split(/\s+/);
+let lastword2=splitedwords2[splitedwords2.length-1]
+let length2 =lastword2.length
+console.log(length2);  
+
+ //Example 3:  
+ //Write a function to check if two strings are anagrams
+ const  words = (word)=> word.split('').sort().join('');
+let compare1 = (words("listen"))
+let comapre2 = (words("silent"))
+
+if (compare1=== comapre2){
+    console.log("Given names are anagram");}
+
+    else{
+       console.log("Given names are not anagram"); 
+    }
+    
+
+
+  
+
