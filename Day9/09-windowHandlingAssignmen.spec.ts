573555583afe33c2f672594b47108dcfb98e09be
AnupriyaRajaram <55028365+AnupriyaRajaram@users.noreply.github.com>
2026-02-12 11:53:55 +0530

    Update 09-windowHandlingAssignmen.spec.ts

--- Day9/09-windowHandlingAssignmen.spec.ts
+++ Day9/09-windowHandlingAssignmen.spec.ts
@@ -38,9 +38,7 @@ const [childPage2] = await Promise.all([context.waitForEvent('page'), page.locat
 //Select the second resulting lead id 
 await childPage2.locator(`(//a[@class='linktext'])[5]`).click();
 await page.bringToFront();
-
-
-
+  
 //Get the message and type of the alert 
 page.on("dialog",async(alert)=>{
 
@@ -48,25 +46,19 @@ const message = alert.message(); //.message() inbuilt method to get the message 
 console.log(message); 
 // accept() => Ok and dismiss() => cancel both are inbuilt methods
 
-
 const alertType = alert.type(); // .type() inbuilt method to get the type of the alert and to handle it accordingly using an if condition
 console.log(`The type of the alert is ${alertType} `);
-
 
 if(alertType==="confirm"){
 await alert.accept()
 }else if(alertType==="prompt"){
 await alert.accept("Testleaf")
 }else
-
-
 await alert.dismiss(); // any other alerts other than "confirm" or "prompt" will be handled here and dismissed
-
 }) 
 
 //Click Merge button 
 await page.locator(`//a[text()='Merge']`).click();
-
-
 }
+
 )
